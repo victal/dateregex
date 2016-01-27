f339834473531cf69f90ca978450f48a95f38ea1
Guilherme Victal <guilhermevictal em gmail ponto com>
2016-01-27 21:36:25 -0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(name='dateregex',
+      version='1.0.0',
+      author='Guilherme Victal',
+      author_email='guilherme at victal.eti.br',
+      url='https://github.com/victal/dateregex',
+      description='A simple library to generate regex that match posterior or anterior dates',
+      license='MIT',
+      packages=['dateregex']
+        )
